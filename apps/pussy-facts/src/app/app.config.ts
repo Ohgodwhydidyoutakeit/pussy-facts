777be80f430599e8a7f7d921409446bfc62b04a3
Ohgodwhydidyoutakeit <w.lipowski97@gmail.com>
2025-04-06 13:26:22 +0200

    feat(product): ngrx and devtools

--- apps/pussy-facts/src/app/app.config.ts
+++ apps/pussy-facts/src/app/app.config.ts
@@ -1,10 +1,16 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideExperimentalZonelessChangeDetection(), // this is experimental api 
     provideRouter(appRoutes),
-  ],
+    provideStore(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(), 
+    })
+],
 };
